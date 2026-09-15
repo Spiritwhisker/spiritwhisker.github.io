a5d95f66fd88907ca4d1a3ba84468d79b375abd4
Spiritwhisker <spiritmist31415926@gmail.com>
2026-09-15 15:58:14 +0000

    new file:   aboutUs.html modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const message = document.querySelector("#message");
 document.getElementById("startHere").addEventListener("click", function() {
     window.location.href = "homepage.html";
 });
+
+document.getElementById("theWeirdAndWorryingAboutUsButton").addEventListener("click", function() {
+    window.location.href = "aboutUs.html";
+});
