5e2b05914acf5418c1aecdf21f355b41c59df40c
Spiritwhisker <spiritmist31415926@gmail.com>
2026-09-21 15:51:03 +0000

    modified:   homepage.html modified:   script.js modified:   workTimer.html modified:   index.html

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # This app
 
-More Time is Needed (or not)
+Please Stop Wasting Your Time (with dino game or smth)
 This is so you don't waste too much time
 
 ## Files
 
 - `index.html`
--
+- `homepage.html`
+- `aboutUs.html`
+- `workTimer.html`
 - `style.css`
 - `script.js`
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const startHere = document.querySelector("#startHere");
 const aboutUsButton = document.querySelector("#theWeirdAndWorryingAboutUsButton");
 const dailyScore = document.querySelector("#dailyScore");
-
+const dailyQualityText = document.querySelector("#dailyQualityText")
+const testAddTen = document.querySelector("#testAddTen")
 
 startHere?.addEventListener("click", () => {
   window.location.href = "homepage.html";
@@ -16,3 +17,14 @@ if (dailyScore) {
   dailyScore.textContent = String(dayScore);
 }
 //Thanks, AI!
+
+let dayQualityText = "Null (No Data, sorry &#x2639;&#xFE0F;)";
+if (dailyScore > 75) {
+  dayQualityText = "Good Job Today!"
+}
+//Meow
+
+
+testAddTen?.addEventListener("click", () => {
+  dayScore += 10;
+})
